6de938d15a62f088e423db74c455d0390e9f9041
Sergey <stvbox@yandex.ru>
2024-03-16 22:48:11 +0300

    + спинер при запуске

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,4 +1,5 @@
-import { React, useCallback, useState, useEffect } from "react";
+import { useCallback, useState, useEffect } from "react";
+import logo from "../../logo.svg";
 import AppHeader from "../app-header/app-header";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
@@ -8,18 +9,24 @@ import style from "./app.module.css";
 const INGRIDIENTS_URL = "https://norma.nomoreparties.space/api/ingredients";
 
 function App() {
-  const [state, setState] = useState({
-    categories: { bun: [], main: [], sauce: [] },
-    ingridients: [],
-    error: null,
+  const [dataUrl, setDataUrl] = useState(null);
+  const [error, setError] = useState(null);
+  const [categories, setCategories] = useState({
+    bun: [],
+    main: [],
+    sauce: [],
   });
+  const [ingridients, setIngridients] = useState([]);
 
-  const loadIngridients = () => {
-    console.log(" --- ");
-    //this.setState({ ...this.state, hasError: false, isLoading: true });
-    fetch(INGRIDIENTS_URL)
+  console.log("...renderApp0");
+
+  function loadIngridients() {
+    if (!dataUrl) {
+      return;
+    }
+
+    fetch(dataUrl)
       .then((result) => {
-        //console.log("then1: ", result);
         return result.json();
       })
       .then((result) => {
@@ -30,21 +37,30 @@ function App() {
           },
           { bun: [], main: [], sauce: [] }
         );
-        setState({ ...state, categories, ingridients: result.data });
+        setIngridients(result.data);
+        setCategories(categories);
       })
       .catch((error) => {
-        setState({ ...state, error });
+        setError(error);
       });
-  };
+  }
 
   useEffect(() => {
+    setDataUrl(INGRIDIENTS_URL);
     loadIngridients();
-  }, []);
+  }, [dataUrl]);
 
-  const closeModalHandler = useCallback((e) => {
-    setState({ ...state, error: null });
-    loadIngridients();
-  }, []);
+  const closeModalHandler = useCallback(
+    (e) => {
+      setError(null);
+      loadIngridients();
+    },
+    [dataUrl]
+  );
+
+  if (ingridients.length < 1) {
+    return <img src={logo} className={style["App-logo"]} alt="logo" />;
+  }
 
   return (
     <>
@@ -52,18 +68,18 @@ function App() {
         <AppHeader />
         <main>
           <section className="section-wrapper pb-10">
-            <BurgerConstructor categories={state.categories} />
+            <BurgerConstructor categories={categories} />
           </section>
           <section className="section-wrapper ml-10">
-            <BurgerIngredients categories={state.categories} />
+            <BurgerIngredients categories={categories} />
           </section>
         </main>
       </div>
-      {state.error && (
+      {error && (
         <Modal title="Ошибка получения данных" closeHandler={closeModalHandler}>
-          <p className="text text_type_main-medium m-3">{INGRIDIENTS_URL}</p>
+          <p className="text text_type_main-medium m-3">{dataUrl}</p>
           <p className="text text_type_main-medium">
-            {state.error.name}: {state.error.message}
+            {error.name}: {error.message}
           </p>
         </Modal>
       )}
